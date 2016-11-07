78761dca3d6e936dc1c27fd993d3f5eaa45868c1
CalebNSmith <Caleb@Calebs-MacBook-Pro.local>
2016-11-06 22:43:13 -0500

    Round 2

--- grab_image.py
+++ grab_image.py
@@ -14,55 +14,66 @@ class comic_image:
         comics = ""
         date = datetime.date.today()
         index = 0
-        root = tk.Toplevel()
+        root = tk.Tk()
         http = urllib3.PoolManager()
         def __init__(self, arg):
             self.comics = arg
 
         def on_key_press(self, event):
-            self.root.destroy()
-            print(ord(event.char))
 
+            self.root.destroy()
+            self.root = tk.Tk()
             if ord(event.char) == 63232:
                 self.date += datetime.timedelta(days=1)
 
             if ord(event.char) == 63233:
                 self.date -= datetime.timedelta(days=1)
 
-            if ord(event.char) == 63234:
+            if ord(event.char) == 63235:
                 self.index += 1
-                if self.index > len(self.comics):
+                if self.index >= len(self.comics):
                     self.index = 0
 
-            if ord(event.char) == 63235:
+            if ord(event.char) == 63234:
                 self.index -= 1
                 if self.index < 0:
                     self.index = -1
-        
+
             self.display_image()
 
         def get_url(self):
-            return "http://www.gocomics.com/" + self.comics[self.index] + "/" + self.date.strftime('%Y') + "/" + self.date.strftime('%M') + "/" + self.date.strftime('%d')
+            return "http://www.gocomics.com/" + self.comics[self.index] + "/" + self.date.strftime('%Y') + "/" + self.date.strftime('%m') + "/" + self.date.strftime('%d')
 
         def display_image(self):
             r = self.http.request('GET', self.get_url())
+            print(self.get_url())
             strip = str(r.data).find("class=\"strip\" src=\"")
-            width = str(r.data).find("\" width=\"600\" /><sp")
             start_index = strip + len("class=\"strip\" src=\"")
 
-            image_url = str(r.data)[start_index: width]
+            end_index = 0
+            while(str(r.data)[start_index + end_index] != "\""):
+                end_index += 1
+
+            image_url = str(r.data)[start_index: start_index + end_index]
 
             response = requests.get(image_url)
+            screen_height = self.root.winfo_screenheight()
+            screen_width = self.root.winfo_screenwidth()
+
             img = Image.open(BytesIO(response.content))
             tk_img = ImageTk.PhotoImage(img)
             width = tk_img.width()
             height = tk_img.height()
             self.root.bind('<KeyPress>', test.on_key_press)
+
             canvas = tk.Canvas(self.root, width=width, height=height)
+            canvas.delete("all")
             canvas.pack()
             canvas.create_image(width / 2, height / 2, image=tk_img)
+            canvas.canvasx(0)
+            canvas.canvasy(0)
 
-            self.root.mainloop()
+            self.root.mainloop()    
 
 
 
