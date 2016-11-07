d5628acf4b94d7dd6f14e4ea57f342a49fee8e80
Alex Colegrove <alexcolegrove93@gmail.com>
2016-11-06 20:56:47 -0500

    Update grab_image.py

--- grab_image.py
+++ grab_image.py
@@ -1,34 +1,72 @@
+#Alex_Colegrove CS1111, Fall 2016
+
 __author__ = 'Caleb'
 import urllib3
+import datetime
 from PIL import Image, ImageTk
 import tkinter as tk
 import requests
 from io import BytesIO
 
-comic_url = "calvinandhobbes"
-year_url = "2014"
-month_url = "07"
-day_url = "01"
-url = "http://www.gocomics.com/" + comic_url + "/" + year_url + "/" + month_url + "/" + day_url
+url_lst =  ['calvinandhobbes', 'garfield','nonsequitur' ]
 
-http = urllib3.PoolManager()
-r = http.request('GET', url)
-strip = str(r.data).find("class=\"strip\" src=\"")
-width = str(r.data).find("\" width=\"600\" /><sp")
-start_index = strip + len("class=\"strip\" src=\"")
+class comic_image:
+        comics = ""
+        date = datetime.date.today()
+        index = 0
+        root = tk.Toplevel()
+        http = urllib3.PoolManager()
+        def __init__(self, arg):
+            self.comics = arg
 
-image_url = str(r.data)[start_index : width]
+        def on_key_press(self, event):
+            self.root.destroy()
+            print(ord(event.char))
 
-response = requests.get(image_url)
-img = Image.open(BytesIO(response.content))
+            if ord(event.char) == 63232:
+                self.date += datetime.timedelta(days=1)
 
-root = tk.Tk()
-tk_img = ImageTk.PhotoImage(img)
-width = tk_img.width()
-height = tk_img.height()
+            if ord(event.char) == 63233:
+                self.date -= datetime.timedelta(days=1)
 
-canvas = tk.Canvas(root, width=width, height=height)
-canvas.pack()
-canvas.create_image(width/2, height/2, image=tk_img)
+            if ord(event.char) == 63234:
+                self.index += 1
+                if self.index > len(self.comics):
+                    self.index = 0
 
-root.mainloop()
+            if ord(event.char) == 63235:
+                self.index -= 1
+                if self.index < 0:
+                    self.index = -1
+        
+            self.display_image()
+
+        def get_url(self):
+            return "http://www.gocomics.com/" + self.comics[self.index] + "/" + self.date.strftime('%Y') + "/" + self.date.strftime('%M') + "/" + self.date.strftime('%d')
+
+        def display_image(self):
+            r = self.http.request('GET', self.get_url())
+            strip = str(r.data).find("class=\"strip\" src=\"")
+            width = str(r.data).find("\" width=\"600\" /><sp")
+            start_index = strip + len("class=\"strip\" src=\"")
+
+            image_url = str(r.data)[start_index: width]
+
+            response = requests.get(image_url)
+            img = Image.open(BytesIO(response.content))
+            tk_img = ImageTk.PhotoImage(img)
+            width = tk_img.width()
+            height = tk_img.height()
+            self.root.bind('<KeyPress>', test.on_key_press)
+            canvas = tk.Canvas(self.root, width=width, height=height)
+            canvas.pack()
+            canvas.create_image(width / 2, height / 2, image=tk_img)
+
+            self.root.mainloop()
+
+
+
+
+test = comic_image(url_lst)
+test.display_image()
+
